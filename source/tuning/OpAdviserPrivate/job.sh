097ccd7a26e2f396b9f3f25fb41ea7dc1a3bec3e
seokjeongeum <jeseok@dblab.postech.ac.kr>
2025-10-13 23:08:54 +0000

    Update job.sh script to change download URL for IMDb dataset and mark join-order-benchmark subproject as dirty. This ensures the script retrieves the dataset from the correct source and reflects the current state of the subproject.

--- source/tuning/OpAdviserPrivate/job.sh
+++ source/tuning/OpAdviserPrivate/job.sh
@@ -4,7 +4,7 @@ git clone https://github.com/seokjeongeum/join-order-benchmark.git
 cd join-order-benchmark || exit
 mv -f ./csv_files/schematext.sql ./csv_files/schematext_backup.sql
 apt install -y wget
-wget http://homepages.cwi.nl/~boncz/job/imdb.tgz
+wget http://event.cwi.nl/da/job/imdb.tgz
 sudo tar -zxvf imdb.tgz -C csv_files
 mv -f ./csv_files/schematext_backup.sql ./csv_files/schematext.sql
 mysql -u root -ppassword < csv_files/schematext.sql
